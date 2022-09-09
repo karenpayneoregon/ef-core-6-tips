532de6a30a5db425fda738088f42f77a3cc961ae
Karen Payne <paynekaren@comcast.net>
2022-09-09 15:44:51 -0700

    porting from 5 to 6

--- EntityFrameworkCoreHelpers/EntityExtensions.cs
+++ EntityFrameworkCoreHelpers/EntityExtensions.cs
@@ -50,6 +50,47 @@ public static class EntityExtensions
         context.Model.GetEntityTypes().Select(eType => eType.ClrType)
             .FirstOrDefault(type => type.Name == modelName);
 
+
+    /// <summary>
+    /// Get details for a model
+    /// </summary>
+    /// <param name="context">Active dbContext</param>
+    /// <param name="modelName">Model name in context</param>
+    /// <returns>List&lt;SqlColumn&gt;</returns>
+    /// <remarks>
+    /// More information can be added as needed
+    /// </remarks>
+    public static List<SqlColumn> GetModelProperties(this DbContext context, string modelName)
+    {
+
+        if (context == null) throw new ArgumentNullException(nameof(context));
+
+        var entityType = GetEntityType(context, modelName);
+
+        var list = new List<SqlColumn>();
+
+        IEnumerable<IProperty> properties = context.Model
+            .FindEntityType(entityType ?? throw new InvalidOperationException())!
+            .GetProperties();
+
+        foreach (IProperty itemProperty in properties)
+        {
+            SqlColumn sqlColumn = new SqlColumn
+            {
+                Name = itemProperty.Name,
+                IsPrimaryKey = itemProperty.IsKey(),
+                IsForeignKey = itemProperty.IsForeignKey(),
+                IsNullable = itemProperty.IsColumnNullable()
+            };
+
+
+            list.Add(sqlColumn);
+
+        }
+
+        return list;
+
+    }
     /// <summary>
     /// Get properties for a model
     /// </summary>
--- EntityFrameworkCoreHelpers/readme.md
+++ EntityFrameworkCoreHelpers/readme.md
@@ -4,8 +4,10 @@ Contains several extension methods for DbContext.
 
 | Method        |   Description    |
 |:------------- |:-------------|
-|  |  |
-|  |  |
+| Comments | Get comments for properties in a model |
+| CanConnectAsync | Determine if a connection can be made with a database with exception handling |
+| GetModelNames | Get model types for models in a DbContext |
+| GetModelPropertyNames | Get property names for a model |
 |  |  |
 
 More methods will be added
