cc08fb49abe531f7a6bd045eeb877f4641f024ac
Karen Payne <paynekaren@comcast.net>
2023-05-25 07:10:53 -0700

    to ef core 7

---
+++ NorthWindContactsApp/Classes/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.CompilerServices;
+using static ConsoleHelperLibrary.Classes.WindowUtility;
+
+// ReSharper disable once CheckNamespace
+namespace NorthWindContactsApp;
+
+internal partial class Program
+{
+    [ModuleInitializer]
+    public static void Init()
+    {
+        Console.Title = "Code sample";
+        SetConsoleWindowPosition(AnchorWindow.Center);
+        Console.WriteLine();
+    }
+}
